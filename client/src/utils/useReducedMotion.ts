979a283561931f47c40569b9bd10c163e0593866
David Jozwik <david@djoz.us>
2025-01-07 01:46:18 -0600

    Small simplification for reduced motion hook

--- client/src/utils/useReducedMotion.ts
+++ client/src/utils/useReducedMotion.ts
@@ -6,24 +6,25 @@ import {
 } from "react";
 
 const QUERY = "(prefers-reduced-motion: no-preference)";
+
 export const usePrefersReducedMotion = () => {
-	// Default to no-animations, since we don't know what the
-	// user's preference is on the server.
-	const [
-		prefersReducedMotion,
-		setPrefersReducedMotion
-	] = useState( true );
+	/*
+		Default to no-animations, since we don't know what the
+		user's preference is on the server.
+	*/
+	const [ prefersReducedMotion, setPrefersReducedMotion ] = useState( true );
+
 	useEffect(
 		() => {
 			const mediaQueryList = window.matchMedia( QUERY );
-			// Set the true initial value, now that we're on the client:
-			setPrefersReducedMotion(
-				!window.matchMedia( QUERY ).matches
-			);
-			// Register our event listener
+
+			// Set the correct initial value on client mount
+			setPrefersReducedMotion( !mediaQueryList.matches );
+
 			const listener = ( event: MediaQueryListEvent ) => {
 				setPrefersReducedMotion( !event.matches );
 			};
+
 			mediaQueryList.addEventListener(
 				"change",
 				listener
