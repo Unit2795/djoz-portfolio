779c1594f884f5e26970bd406f7c3090704cc86c
David Jozwik <david@djoz.us>
2025-01-12 22:08:25 -0600

    Remove glass button variants

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,31 +1,23 @@
 import clsx from "clsx";
-import {
-	ButtonProps
-} from "@/components/Button/types.ts";
 import "./Button.css";
+import {
+	ButtonHTMLAttributes
+} from "react";
 
 const Button = (
 	{
 		children,
 		className,
-		variant = "neutral",
 		type = "button",
 		...rest
-	}: ButtonProps
+	}: ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-	const variantClass = {
-		primary: "bg-primary text-white",
-		secondary: "bg-secondary text-white",
-		neutral: "bg-white/5 text-neutral"
-	};
-
 	return (
 		<button
 			className={
 				clsx(
-					variantClass[ variant ],
 					className,
-					"px-6 py-3 backdrop-blur-sm rounded-lg font-medium border border-white/20 shadow-lg hover:bg-white/10 transition-colors duration-200 glass-btn"
+					"bg-white/5 text-neutral px-6 py-3 backdrop-blur-sm rounded-lg font-medium border border-white/20 shadow-lg hover:bg-white/10 transition-colors duration-200 glass-btn"
 				)
 			}
 			type={
--- client/src/components/Button/types.ts
+++
@@ -1,7 +0,0 @@
-import {
-	ButtonHTMLAttributes
-} from "react";
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	variant?: "primary" | "secondary" | "neutral"
-}
