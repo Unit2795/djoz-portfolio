70c201af17d474adfbd53508c796bc93e27a11e4
David Jozwik <david@djoz.us>
2025-09-20 01:28:34 -0500

    testing issue with local vs build on analytics route handler

--- client/src/pages/analytics.ts
+++ client/src/pages/analytics.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
-	console.log(await request.json());
-
-	return new Response(null, { status: 200 });
+	// This will only work in development
+	if (request.headers.get("Content-Type") === "application/json") {
+		console.log(await request.json());
+		return new Response(null, { status: 200 });
+	}
+	// In production, return 404 or redirect
+	return new Response("Not found", { status: 404 });
 };
